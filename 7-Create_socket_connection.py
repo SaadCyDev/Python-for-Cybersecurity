3bc6cba7eb26734ebdb1703d3846041404026dc5
SaadCyDev <sarwaqoali2022@gmail.com>
2024-03-25 23:08:19 +0300

    Create a socket connection

---
+++ 7-Create_socket_connection.py
@@ -0,0 +1,18 @@
+import socket 
+
+def main():
+    s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = 'www.google.com'
+    port = 80
+    try:
+       result= s.connect_ex((host,port))
+       if result==0:
+           print("Successfully")
+       else:
+           print("connection refused")
+       s.close()
+    except Exception as e:
+        print("error") 
+        
+if __name__=='__main__':
+    main()
