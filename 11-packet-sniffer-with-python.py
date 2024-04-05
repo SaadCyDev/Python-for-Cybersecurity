a7ec1e3a7dd1121ee0f906bec8f5b2c2bf627cb8
SaadCyDev <sarwaqoali2022@gmail.com>
2024-04-06 00:56:43 +0300

    packet sniffer using oython

--- 11-packet-sniffer-with-python.py
+++ 11-packet-sniffer-with-python.py
@@ -1,11 +1,15 @@
 import socket 
+from packet_capture_output import PCAPFile
 
 def main():
     conn= socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    pcap= PCAPFile('packets.pcap')
     while True:
-       raw_data, addr= conn.recvfrom(65500) 
-       print(raw_data)
-       print(addr)
+       raw_data, addr= conn.recvfrom(65535) 
+    #    print(raw_data)
+    #    print(addr)
+       pcap.write(raw_data)
+    pcap.close()
     
 if __name__=='__main__':
     main()
